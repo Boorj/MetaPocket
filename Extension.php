a026321c628652456531748f6dc4c1719257b806
Boorj <Boorj@users.noreply.github.com>
2015-11-13 17:05:24 +0600

    removed route_meta_contexted (now it detects arguments), some refactoring, added print functions

--- Extension.php
+++ Extension.php
@@ -23,18 +23,30 @@ class Extension extends BaseExtension
         $this->app['metapocket'] = $this->app->share(function ($app) { return new MetaPocket($app); });
         $this->app['metapocket']->initialize();
 
-        $this->addTwigFunction('site_meta', 'get_site_meta');
-        $this->addTwigFunction('route_meta', 'get_route_meta');
-        $this->addTwigFunction('route_meta_contexted', 'get_route_meta_contexted');
+        $this->addTwigFunction('site_meta',            [$this->app['metapocket'], 'get_meta_for_site'       ]);
+        $this->addTwigFunction('route_meta',           [$this->app['metapocket'], 'get_meta_for_route'      ]);
+//        $this->addTwigFunction('route_meta_contexted', [$this->app['metapocket'], 'get_meta_for_route_contexted']);
+
+        $this->addTwigFunction('print_site_meta',      'print_site_meta'  );
+        $this->addTwigFunction('print_route_meta',     'print_route_meta' );
     }
 
-    public function get_site_meta($meta_or_sequence) {
-        return $this->app['metapocket']->get_site_meta($meta_or_sequence);
+//    public function get_meta_for_site($meta_or_sequence) {
+//        return $this->app['metapocket']->get_meta_for_site($meta_or_sequence);
+//    }
+//    public function get_meta_for_route($seq, $route = false) {
+//        return $this->app['metapocket']->get_meta_for_route($seq, $route);
+//    }
+//
+    public function print_site_meta($meta_or_sequence) {
+        $str = $this->app['metapocket']->print_site_meta($meta_or_sequence);
+        return new \Twig_Markup($str, 'UTF-8');
     }
-    public function get_route_meta($seq, $route = false) {
-        return $this->app['metapocket']->get_route_meta($seq, $route);
+    public function print_route_meta($seq, $route = false) {
+        $str = $this->app['metapocket']->print_route_meta($seq, $route);
+        return new \Twig_Markup($str, 'UTF-8');
     }
-    public function get_route_meta_contexted($seq, $context, $route = false) {
-        return $this->app['metapocket']->get_route_meta_contexted($seq, $context, $route);
-    }
+//    public function get_meta_for_route_contexted($seq, $context, $route = false) {
+//        return $this->app['metapocket']->get_meta_for_route_contexted($seq, $context, $route);
+//    }
 }
--- MetaPocket.php
+++ MetaPocket.php
@@ -1,25 +1,25 @@
 <?php
 /*
  * 10.11.2015 14:50  ! created
+ *
+ * @todo list of auto print metas for each route.
+ * @todo join record data and other data (route, site).
+ *       maybe do that from template like join_record_seo_data_to_current_route
+ *       then in template you can use joined data. like records.current
+ *
  * */
 
 namespace Bolt\Extension\Mapple\MetaPocketExtension;
 
 use Bolt\Application;
 use Bolt\BaseExtension;
+use Mapple\Site\Logic\Utils;
 use Symfony\Component\Process\Exception\LogicException;
 use Symfony\Component\Yaml\Parser;
 use Bolt\Helpers\Arr;
 
 class MetaPocket
 {
-    /** @var Application $app   */
-    private $app = null;
-
-    /** @var \Symfony\Component\Yaml\Parser */
-    protected $yamlParser = false;
-
-    protected $metas = null;
 
     const DELIMITER          = '.';
     const GLOBAL_PATH        = 'global';
@@ -27,14 +27,58 @@ class MetaPocket
     const METAS_OVER_ROUTING = true;
     const INHERIT_METAS      = true;
 
-    public $var_tags = [];
-    public $default_settings = [];
+    /** @var Application $app   */
+    private $app = null;
+
+    /** @var \Symfony\Component\Yaml\Parser */
+    private $yamlParser = false;
+
+    private $metas = null;
+
+    private $var_tags = [];
+
+    private $default_settings = [];
 
 
-
+    /**
+     * __construct
+     * @param $app
+     */
     function __construct($app) {
         $this->app = $app;
+    }
 
+
+    /**
+     * initialize
+     */
+    public function initialize()
+    {
+        //initialize default settings
+        $this->default_setup();
+
+        // get data from config.yml and merge
+        $this->merge_with_meta_yml();
+
+
+        $this->replace_available_vars_in_global();
+
+
+        // for each route take some data from 'meta' and 'label' fields.
+        $this->inherit_from_routing_yml();
+
+        $this->fill_blank_fields_for_routes();
+
+        //make replacing sequence for selected fields in global's and routes' metas
+        $this->replace_available_vars_in_routes();
+
+    }
+
+
+    /**
+     * default_setup
+     */
+    private function default_setup() {
         $this->var_tags = ['open'=>'%', 'close'=>'%'];
 
         $this->default_settings = [
@@ -49,48 +93,131 @@ class MetaPocket
                     ],
                 ],
                 'meta' => [
+                    'robots' => '',
                     'description' => '',
                     'keywords' => '',
                     'share' => [
                         'heading' => '',
                         'comment' => '',
                         'comment_short' => '',
-                        'image' => '',
+                        'image' => 'http://%global.siteurl%/i/screenshot.jpg',
                     ],
                 ],
             ],
             'routes' => [
                 'home' => [
-                    'label' => '%parts.sitelabel%',
+                    'title' => '%parts.sitelabel%',
                 ],
             ],
         ];
+
+        // @todo Make a list of what to convert - for globals and for each route.
+
+        //        <meta name="robots" content="noindex, follow">
+        //    — содержимое страницы индексировать нельзя, но следовать по ссылкам можно
+        //        <meta name="robots" content="index, nofollow">
+        //    — разрешается индексировать содержимое страницы, но не следовать по ссылкам, которые робот на ней обнаружит
+        //        <meta name="robots" content="noindex, nofollow">
+        //    — запрещается и индексировать содержимое страницы, и следовать по ссылкам
+        //        <meta name="robots" content="index, follow">
+        //    — разрешается и индексировать содержимое страницы, и следовать по ссылкам.
+    }
+
+
+    /**
+     * merge_with_meta_yml
+     */
+    private function merge_with_meta_yml() {
+        $this->metas = Arr::mergeRecursiveDistinct(
+            $this->default_settings,
+            $this->parseConfigYaml('meta.yml')
+        );
+    }
+
+
+    /**
+     * inherit_from_routing_yml
+     */
+    protected function inherit_from_routing_yml () {
+        $global_meta = $this->metas['global']['meta'];
+        $routes    = &$this->metas['routes'];
+
+
+        // getting meta data from routing.yml
+        $routings = $this->app['config']->get('routing');
+        foreach ($routings as $yml_route_name => $yml_route_data) {
+            if (!isset($routes[$yml_route_name]))
+                $routes[$yml_route_name] = [];
+
+            // @todo fill titles too
+
+            // if NOT set <route>.meta in meta.yml, taking from global metas
+            if (!isset($routes[$yml_route_name]['meta'])) {
+                $routes[$yml_route_name]['meta'] = $global_meta;
+            }
+
+            // if ISset <route>.meta in routing.yml - merging with data for <route>.meta
+            if (isset($yml_route_data['meta'])) {
+                $routes[$yml_route_name] = (self::METAS_OVER_ROUTING)
+                    ? array_merge($yml_route_data['meta'], $routes[$yml_route_name])
+                    : array_merge($routes[$yml_route_name], $yml_route_data['meta']);
+            }
+
+            // if ISset <route>.label in routing.yml - if it's set as <route>.label, not as <route>.META.label)
+            if (isset($yml_route_data['label'])) {
+                if (!isset($routes[$yml_route_name]['label']) || !self::METAS_OVER_ROUTING) {
+                    $routes[$yml_route_name]['label'] = $yml_route_data['label'];
+                }
+            }
+        }
+    }
+
+
+    protected function replace_available_vars_in_global () {
+        $this->replace_value(self::VARS_PATH.  '.page_title.default_pattern');
+        $this->replace_value(self::GLOBAL_PATH.'.meta.share.image');
+
+    }
+
+
+    private function fill_blank_fields_for_routes() {
+        $default_pattern = $this->get_meta_value_by_seq(self::VARS_PATH.'.page_title.default_pattern');
+        $default_image  = $this->get_meta_value_by_seq(self::GLOBAL_PATH.'.meta.share.image');
+
+        foreach ($this->metas['routes'] as $route_name => $route) {
+            if (empty($route['title'])) {
+                $this->set_meta_value("routes.{$route_name}.title", $default_pattern);
+            }
+            $image = $this->get_meta_value_by_seq("routes.{$route_name}.meta.share.image");
+            if (empty($image)) {
+                $this->set_meta_value("routes.{$route_name}.meta.share.image", $default_image);
+            }
+        }
     }
 
 
 
     /**
-     * replace_available_vars_in_metas
+     * replace_available_vars_in_routes
      */
-    protected function replace_available_vars_in_metas () {
-
-        $default_pattern = $this->replace_value(self::VARS_PATH.'page_title.default_pattern');
-
+    protected function replace_available_vars_in_routes () {
 
         foreach ($this->metas['routes'] as $route_name => $route) {
-            if (empty($route['title'])) {
-                $this->set_meta_value_by_seq("routes.{$route_name}.title", $default_pattern);
-            }
-            $add_vars = (!empty($route['label'])) ? [
-                'label' => $route['label'],
-//                    'context' => ['post'=>['title' => 'TUTLE']]
-            ] : [];
-            $this->replace_value("routes.{$route_name}.title", $add_vars);
+            $this->replace_value("routes.{$route_name}.title", $this->get_meta_value_by_seq("routes.{$route_name}"));
+
+//            $add_vars = (!empty($route['label'])) ? [
+//                'label' => $route['label'],
+////                    'context' => ['post'=>['title' => 'TUTLE']]
+//            ] : [];
+//            $this->replace_value("routes.{$route_name}.title", $add_vars);
 
         }
 
 //        dump($this->metas);
     }
+
+
+    //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
 
     /**
@@ -133,6 +260,8 @@ class MetaPocket
         $value = null;
         $seq = false;
 
+
+        // if there's a delimiter ("." by default) in string
         $first_delimiter_pos = strpos($var_slug, $delimiter);
         if ($first_delimiter_pos !== false  &&  $first_delimiter_pos > 0) {
             $split_var_slug = explode($delimiter, $var_slug);
@@ -140,6 +269,10 @@ class MetaPocket
             $rest_part = substr($var_slug, $first_delimiter_pos + 1);
 
             switch ($first_part) {
+                case 'global'   :
+                    $value = $this->get_meta_value_by_seq( self::GLOBAL_PATH . "." . $rest_part );
+                    break;
+
                 case 'vars'   :
                     $value = $this->get_meta_value_by_seq( self::VARS_PATH . "." . $rest_part );
                     break;
@@ -153,8 +286,16 @@ class MetaPocket
                         $value = $this->_get_value($additional_vars['context'], $rest_part, null);
                     }
                     break;
+
+                // @todo - get bolt data
+//                case 'bolt':
+//                    if (!empty($additional_vars['context'])) {
+//                        $value = $this->_get_value($additional_vars['context'], $rest_part, null);
+//                    }
+//                    break;
             }
         }
+
         if ($seq) {
 
         }
@@ -199,11 +340,11 @@ class MetaPocket
 
 
     /**
-     * set_meta_value_by_seq
+     * set_meta_value
      * @param $str
      * @param $value
      */
-    protected function set_meta_value_by_seq($str, $value) {
+    protected function set_meta_value($str, $value) {
         $this->_set_value($this->metas, $str, $value);
     }
 
@@ -232,7 +373,8 @@ class MetaPocket
 
 
     /**
-     * replace_value
+     * ### replace_value
+     * gets value by sequence, replaces
      * @param       $seq
      * @param array $additional_vars
      * @return mixed
@@ -241,8 +383,7 @@ class MetaPocket
         $value = $this->get_meta_value_by_seq($seq);
         $new_value = $this->replace_vars_string($value, $additional_vars);
         if ($new_value !== $value)
-            $this->set_meta_value_by_seq($seq, $new_value);
-
+            $this->set_meta_value($seq, $new_value);
         return $new_value;
     }
 
@@ -280,7 +421,7 @@ class MetaPocket
     }
 
 
-    private function get_all_route_metas ($route = false) {
+    private function get_all_metas_for_route ($route = false) {
         if (!$route) {
             $route = $this->app['request_stack']->getCurrentRequest()->get('_route');
         }
@@ -291,7 +432,7 @@ class MetaPocket
     }
 
 
-    protected function get_seq_by_meta ($meta) {
+    protected function get_seq_by_meta_name ($meta) {
         $seq = '';
         switch ($meta) {
             case 'title' :
@@ -314,6 +455,28 @@ class MetaPocket
         return $seq;
     }
 
+    protected function print_meta ($meta, $data) {
+        switch ($meta) {
+            case 'title'    : return
+                '<title>'.trim($data).'</title>';
+
+            case 'keywords' :
+                if (is_array($data) || is_string($data)) {
+                    $keywords = (is_string($data) ? trim($data) : implode(', ', $data));
+                    return '<meta name="keywords" content="' . $keywords . '"/>';
+                }
+                break;
+
+            case 'image'    :
+                return
+                    '<meta itemprop="image" content="'.$data.'">'
+                   .'<link rel="image_src" href="'    .$data.'">';
+
+            case 'description'    :
+                return '<meta name="description" content="'.$data.'">';
+        }
+        return '';
+    }
 
 
     //——————————————————————————————————————————————————————————————————————
@@ -321,103 +484,92 @@ class MetaPocket
     //——————————————————————————————————————————————————————————————————————
 
 
-    /**
-     * initialize
-     */
-    public function initialize()
-    {
-        $app    = &$this->app;
+    public function print_route_meta ($seq, $route = false) {
+        $meta = $this->get_meta_for_route($seq, $route);
 
-        $this->metas = Arr::mergeRecursiveDistinct(
-            $this->default_settings,
-            $this->parseConfigYaml('meta.yml')
-        );
+        return ($meta) ? $this->print_meta($seq, $meta) : '';
+    }
 
-        $metas  = &$this->metas;
-        $routes = &$metas['routes'];
-
-        $site_meta = $metas['global']['meta'];
-
-        // getting meta data from routing.yml
-        $routings = $app['config']->get('routing');
-        foreach ($routings as $route_name => $route_data) {
-            if (!isset($routes[$route_name]))
-                $routes[$route_name] = [];
-
-            // @todo fill titles too
-
-            // if not set meta data for route in meta.yml, taking from global metas
-            if (!isset($routes[$route_name]['meta'])) {
-                $routes[$route_name]['meta'] = $site_meta;
-            }
-
-            // if isset route metas in routing.yml - merging
-            if (isset($route_data['meta'])) {
-                if (self::METAS_OVER_ROUTING)
-                    $routes[$route_name] = array_merge($route_data['meta'], $routes[$route_name]);
-                else
-                    $routes[$route_name] = array_merge($routes[$route_name], $route_data['meta']);
-            }
-
-            // getting label from routing.yml : if it's set as routing.<route_data>.label, not as routing.<route_data>.meta.label)
-            if (isset($route_data['label'])) {
-                if (!isset($routes[$route_name]['label']) || !self::METAS_OVER_ROUTING) {
-                    $routes[$route_name]['label'] = $route_data['label'];
-                }
-            }
-        }
-
-        $this->replace_available_vars_in_metas();
+    public function print_site_meta ($seq) {
+        $meta = $this->get_meta_for_route($seq);
+        return ($meta) ? $this->print_meta($seq, $meta) : '';
     }
 
 
     /**
-     * get_site_meta
-     * @param $seq
+     * get_meta_for_site
+     * @param string       $seq - relative or absolute path
+     * @param bool|false   $root  - ["global" by default] - "global" or "routes.post" for example
      * @return null
      */
-    public function get_site_meta ($seq) {
+    public function get_meta_for_site ($seq, $root = self::GLOBAL_PATH) {
+
         // @todo reduce this function
         //if seq is not a sequence, but meta-name
         switch ($seq) {
             case 'description' :
             case 'keywords' :
-                $seq = self::GLOBAL_PATH . '.meta.' . $seq;
+                $seq = $root . '.meta.' . $seq;
                 break;
+
             case 'heading' :
             case 'comment' :
             case 'comment_short' :
             case 'image' :
-                $seq = self::GLOBAL_PATH . '.meta.share.' . $seq;
+                $seq = $root . '.meta.share.' . $seq;
                 break;
 
             default :
-                // @todo thinking about this part.. should i restrict that only to globals or not
-                if (strpos($seq, self::GLOBAL_PATH . '.') !== 0) {
-                    $seq = self::GLOBAL_PATH . '.' . $seq;
-                }
+                $seq = false;
         }
+
+        if ($seq === false) {
+            // @todo thinking about this part.. should i restrict that only to globals or not
+            // if not found in other commands and starting with this "<root>." ("global." for example)
+            // add "<root>."
+            if (strpos($seq, "{$root}.") !== 0) {
+                $seq = $root . '.' . $seq;
+            }
+        }
+
         //getting data by sequence
         $data = $this->get_meta_value_by_seq($seq);
         return $data;
     }
 
 
-    public function get_route_meta ($seq, $route = false) {
-        $route_metas = $this->get_all_route_metas($route);
+    public function get_meta_for_route ($seq, $context = false, $route = false) {
+        $args = func_get_args();
+
+        if (count($args) > 1)
+            for ($i=1; $i < count($args); $i++) {
+                $arg = $args[$i];
+                if (is_string($arg))
+                    $route = $arg;
+                else
+                    $context = $arg;
+            }
+
+        $route_metas = $this->get_all_metas_for_route($route);
+
         $is_seq = (strpos($seq, self::DELIMITER) !== false);
 
         if (!$is_seq)
-            $seq = $this->get_seq_by_meta($seq);
+            $seq = $this->get_seq_by_meta_name($seq);
 
         $result = $this->_get_value($route_metas, $seq);
-        return $result;
+
+        return ($context === false)
+            ? $result
+            : $this->replace_vars_string($result, ['context' => $context]);
+
     }
 
-    public function get_route_meta_contexted ($seq, $context, $route = false) {
-        $meta = $this->get_route_meta($seq, $route);
-        return $this->replace_vars_string($meta, ['context' => $context]);
-    }
+
+//    public function get_meta_for_route_contexted ($seq, $context, $route = false) {
+//        $meta = $this->get_meta_for_route($seq, $route);
+//        return $this->replace_vars_string($meta, ['context' => $context]);
+//    }
 
 //    public function get_record_meta ($seq = false, $context = false) {
 //        if ($seq === false  &&  $context === false) {
